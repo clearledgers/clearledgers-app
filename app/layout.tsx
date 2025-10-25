32b1e35ed4f29422464fffb514902313bcc65c32
Anubhav <admin@Anubhavs-MacBook-Air.local>
2025-10-26 02:34:27 +0530

    Fix Tailwind CLI and force CSS inclusion

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
+import "../public/test.css";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-geist-sans" })
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-geist-mono" })
